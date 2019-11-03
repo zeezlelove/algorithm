04f03441f74119ec181de67dfac89471ee39419a
zeezlelove <zeezlelove@gmail.com>
2019-11-03 18:12:58 +0900

    백준 문제풀이

---
+++ 구현 코드/달팽이수열(python).py
@@ -0,0 +1,17 @@
+def snail(n):
+    a = [[0 for i in range(n)] for i in range(n)]
+    i,j,num = 0,-1,1
+    chk = 1
+    while n > 0:
+        for _ in range(n): #행
+            j += chk
+            a[i][j] = num
+            num += 1
+        n -= 1
+        if n == 0:break
+        for _ in range(n): #열
+            i += chk
+            a[i][j] = num
+            num += 1
+        chk *= -1
+    return a
