0d4e48c67ab590d62136a6c01095ee239299a01d
zeezlelove <zeezlelove@gmail.com>
2019-11-03 18:13:28 +0900

    알고리즘 구현

---
+++ 구현 코드/우선순위 큐.py
@@ -0,0 +1,13 @@
+import heapq,sys
+ip = sys.stdin.readline
+heap = []
+for i in range(int(ip())):
+    n = int(ip())
+    if n == 0:
+        if heap:
+            print(abs(heapq.heappop(heap)))
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap,-n)
+#백준 최대힙
