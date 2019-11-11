546315fc3f0cb47e169960dc2f0d7e92e27729d3
zeezlelove <zeezlelove@gmail.com>
2019-11-11 22:40:03 +0900

    알고리즘 구현

--- 구현 코드/Bubble sort.py
+++ 구현 코드/Bubble sort.py
@@ -2,5 +2,5 @@ a = [1,10,5,8,7,6,4,3,2,9]
 for i in range(10):
     for j in range(9-i):
         if a[j] > a[j+1]:
-            a[j],a[j+1] = a[j+1],a[j]
+            a[j], a[j+1] = a[j+1], a[j]
 print(a)
