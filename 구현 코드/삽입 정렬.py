a66d0b3a10538419b96747f547b57dd5466d1367
zeezlelove <zeezlelove@gmail.com>
2019-11-22 21:53:53 +0900

    백준 문제풀이

---
+++ 구현 코드/삽입 정렬.py
@@ -0,0 +1,7 @@
+array = [5,4,1,8,7,6,2,9,3]
+for i in range(len(array)-1):
+    j = i
+    while j >= 0 and array[j] > array[j+1]:
+        array[j],array[j+1] = array[j+1],array[j]
+        j -= 1
+print(array)
