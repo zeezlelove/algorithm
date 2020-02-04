35ef40722e96eba949c983b7456194b8d945bbc4
zeezlelove <zeezlelove@gmail.com>
2020-02-04 22:44:14 +0900

    백준 문제풀이

--- 구현 코드/플로이드 와샬.py
+++ 구현 코드/플로이드 와샬.py
@@ -9,13 +9,13 @@ d = [[inf for i in range(n)] for i in range(n)]
 for i in range(n):
     d[i][i] = 0
 def floydWarshall():
-    #k:거쳐가는 노드
-    for k in range(n):
-        #i:출발노드
-        for i in range(n):
-            #j:도착노드
-            for j in range(n):
-                d[i][j] = min(d[i][k]+d[k][j],d[i][j]) #현재 노드의 가중치(d[i][j])보다 거쳐가는 노드의 가중치(d[i][k]+d[k][j])가 더작다면 갱신
+    #i :거쳐가는 노드
+    for i in range(n):
+        #j:출발노드
+        for j in range(n):
+            #k:도착노드
+            for k in range(n):
+                d[j][k] = min(d[j][i]+d[i][k],d[j][k]) #현재 노드의 가중치(d[j][k])보다 거쳐가는 노드의 가중치(d[j][i]+d[i][k])가 더작다면 갱신
     for i in range(n):
         for j in range(n):
             if d[i][j] == 1e9: #만약에 갈수없으면 0출력
