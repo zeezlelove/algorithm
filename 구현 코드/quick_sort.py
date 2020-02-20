c7ef7c969db9cb6e21dfdaa6f214114b6babfd5b
zeezlelove <zeezlelove@gmail.com>
2020-02-20 15:52:02 +0900

    알고리즘 구현 코드

--- 구현 코드/quick_sort.py
+++ 구현 코드/quick_sort.py
@@ -1,17 +1,16 @@
-arr = list(map(int,input().split()))
+arr = list(map(int,input().split())) # [10 9 8 7 6 5 4 3 2 1]
 def quick_sort(l,r):
+    if l >= r:return
     i,j = l,r
     pivot = arr[(l+r)//2]
-    while arr[i] < pivot:
-        i += 1
-    while arr[j] > pivot:
-        j -= 1
-    if i <= j:
-        arr[i],arr[j] = arr[j],arr[i]
-        i += 1
-        j -= 1
     while i <= j:
-        if l < j:
-            quick_sort(l,j)
-        if i < r:
-            quick_sort(i,r)
+        while arr[i] < pivot: i += 1
+        while arr[j] > pivot: j -= 1
+        if i <= j:
+            arr[i],arr[j] = arr[j],arr[i]
+            i += 1
+            j -= 1
+    quick_sort(l,j)
+    quick_sort(i,l)
+quick_sort(0,len(arr)-1)
+print(arr) #[1 2 3 4 5 6 7 8 9 10]
