1a426bf3bf38e30539908d6956355fcb2b1397ad
zeezlelove <zeezlelove@gmail.com>
2020-03-04 22:09:31 +0900

    알고리즘 구현코드

--- 구현 코드/재귀로 배열 조합 출력하기.py
+++ 구현 코드/재귀로 배열 조합 출력하기.py
@@ -1,16 +1,14 @@
-def func(ix1,ix2):
-    if ix2 == 6:
-        for i in lotto:
+def func(ix,cnt):
+    if cnt == k: #cnt가 k되었으므로 종료하고 출력한다.
+        for i in li:
             print(i,end=' ')
         print()
     else:
-        for i in range(ix1,k):
-            lotto[ix2] = arr[i]
-            func(i+1,ix2+1)
-while 1:
-    lotto = [0 for i in range(6)]
-    arr = list(map(int,input().split()))
-    k = arr.pop(0)
-    if k == 0:break
-    func(0,0)
-    print()
+        for i in range(ix,len(arr)): #자신의 현재값보다 작은값은보지 않는다.
+            li[cnt] = arr[i]
+            func(i+1,cnt+1)
+k = int(input())
+li = [0 for i in range(k)]
+arr = list(map(int,input().split()))
+func(0,0)
+print()
