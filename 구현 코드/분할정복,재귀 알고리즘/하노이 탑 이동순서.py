cfb1e3592d0bad9d79b24f7d2b77fccb8d545657
김승우 <zeezlelove@gmail.com>
2020-04-27 16:56:47 +0900

    알고리즘 구현코드

--- 구현 코드/분할정복,재귀 알고리즘/하노이 탑 이동순서.py
+++ 구현 코드/분할정복,재귀 알고리즘/하노이 탑 이동순서.py
@@ -1,7 +1,6 @@
 def hanoi(num,A,B,C): #A에 꽃혀있는 num개의 원반을 B를 거쳐 C로 이동한다.
     #이동할 원반이 한개라면?
     if num == 1:
-        return
         print("원반 {}을 {}에서 {}로 이동".format(num,A,C))
     else:
         #마지막 한개를 제외하고 A기둥에서 C기둥를 거쳐 B기둥으로 옮긴다.
