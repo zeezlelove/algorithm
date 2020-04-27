7d1ec0206857ed514e2c82803920fc4a2d3d059a
김승우 <zeezlelove@gmail.com>
2020-04-27 20:17:18 +0800

    알고리즘 구현코드

--- 구현 코드/이분탐색/lower_bound 함수 구현.py
+++ 구현 코드/이분탐색/lower_bound 함수 구현.py
@@ -4,10 +4,10 @@ def lower_bound(val):
     s,e,ans = 0,n-1,1e9
     while s <= e:
         mid = (s+e)//2
-        if arr[mid] < val:
+        if arr[mid] < val: #mid값이 찾으려는 값보다 작으면
             s = mid+1
-        else:
-            ans = min(ans,mid)
+        else: #mid값이 찾으려는 값보다 크면
+            ans = min(ans,mid) #갱신
             e = mid-1
     return ans
 print(lower_bound(int(input())))
