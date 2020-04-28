a0d450ad7ee659b1c936b5cd196964ba92ab14d4
김승우 <zeezlelove@gmail.com>
2020-04-28 16:38:02 +0800

    알고리즘 구현코드

---
+++ 구현 코드/이분탐색/lower_bound.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = sorted(list(map(int,input().split())))
+def lower_bound(val):
+    s,e,ans = 0,n-1,1e9
+    while s <= e:
+        mid = (s+e)//2
+        if arr[mid] < val: #mid값이 찾으려는 값보다 작으면
+            s = mid+1
+        else: #mid값이 찾으려는 값보다 크면
+            ans = min(ans,arr[mid]) #갱신
+            e = mid-1
+    return ans
+print(lower_bound(int(input())))
