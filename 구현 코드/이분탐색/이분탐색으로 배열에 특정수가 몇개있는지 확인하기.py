04fd990b029352406765b2bb4462ac898267e75e
김승우 <zeezlelove@gmail.com>
2020-06-27 16:00:38 +0900

    알고리즘 구현코드

--- 구현 코드/이분탐색/이분탐색으로 배열에 특정수가 몇개있는지 확인하기.py
+++ 구현 코드/이분탐색/이분탐색으로 배열에 특정수가 몇개있는지 확인하기.py
@@ -22,4 +22,4 @@ def upper_bound(val):
             e = mid-1
     return e
 print(upper_bound(tar)-lower_bound(tar),end=' ')
-#
+# lower_bound는 tar값의 이상값의 인덱스 upper_bound는 초과값의 인덱스이므로 둘이 upper_bound에서 lower_bound를 빼주면 된다.
