52f91e389041d9084ceb23b21111271fd59b6f30
김승우 <zeezlelove@gmail.com>
2020-07-06 16:38:50 +0900

    알고리즘 구현코드

--- 구현 코드/순열 조합 알고리즘/재귀로 배열 순열 구하기.py
+++ 구현 코드/순열 조합 알고리즘/재귀로 배열 순열 구하기.py
@@ -1,18 +1,18 @@
-def func(cnt):
-    if cnt == k: #cnt가 k되었으므로 종료하고 출력한다.
-        for i in v:
+def dfs():
+    if len(Print) == m: #cnt가 k되었으므로 종료하고 출력한다.
+        for i in Print:
             print(i,end=' ')
         print()
     else:
-        for i in range(len(arr)):
-            if li[i]:continue
-            li[i] = 1
-            v.append(arr[i])
-            func(cnt+1)
-            v.pop()
-            li[i] = 0
-k = int(input())
-li = [0 for i in range(k)]
-v = []
-arr = list(map(int,input().split()))
-func(0)
+        for i in range(1,n+1):
+            if not v[i]:
+                v[i] = 1
+                Print.append(i)
+                dfs()
+                Print.pop()
+                v[i] = 0
+n,m = map(int,input().split())
+v = [0 for i in range(n+1)]
+Print = []
+dfs()
+#백준 N과 M
